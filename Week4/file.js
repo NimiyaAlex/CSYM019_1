4de559429301c90d7ee13b6359f66e6aaefd87cb
NimiyaAlex <nimiyakarathedam@gmail.com>
2021-02-17 15:50:10 +0000

    commit 4

---
+++ Week4/file.js
@@ -0,0 +1,19 @@
+//alert('script loaded');//popup message will generate
+//var b="nim";
+//var a=12;
+//alert(b);
+function print5()
+{
+  var a=1;
+  var b=2;
+  var c=3;
+  var d=4;
+  var e=5;
+  alert(a);
+  alert(b);
+  alert(c);
+  alert(d);
+  alert(e);
+}
+print5();
+print5();
