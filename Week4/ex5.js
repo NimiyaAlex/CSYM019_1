cfabd47c73765e1cc06a7666707afe8e17377e8d
Nimiya Alex <nimiyakarathedam@gmail.com>
2021-02-17 16:39:37 +0000

    exercise5

--- Week4/ex5.js
+++ Week4/ex5.js
@@ -1,22 +1,4 @@
-//alert('script loaded');//popup message will generate
-//var b="nim";
-//var a=12;
-//alert(b);
-function print5()
-{
-  var a=1;
-  var b=2;
-  var c=3;
-  var d=4;
-  var e=5;
-  alert(a);
-  alert(b);
-  alert(c);
-  alert(d);
-  alert(e);
-}
-print5();
-print5();
+
 function myLoadFunction(){
   var element=document.getElementById('pageheading');
   element.firstChild.nodeValue="welcome again";
