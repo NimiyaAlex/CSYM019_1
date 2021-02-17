eb8af39aa0e5665695b56466d0283832288eefe5
NimiyaAlex <nimiyakarathedam@gmail.com>
2021-02-17 16:26:21 +0000

    commit 5

--- Week4/file.js
+++ Week4/file.js
@@ -17,3 +17,11 @@ function print5()
 }
 print5();
 print5();
+function myLoadFunction(){
+  var element=document.getElementById('pageheading');
+  element.firstChild.nodeValue="welcome again";
+
+ var element=document.getElementById('paragraph');
+element.firstChild.nodeValue="new paragraph";
+}
+document.addEventListener('DOMContentLoaded',myLoadFunction);
