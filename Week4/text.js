2eeb28aed2e366f7c37227a706d4299064c65699
NimiyaAlex <>
2021-03-08 14:01:24 +0000

    ex7

---
+++ Week4/text.js
@@ -0,0 +1,13 @@
+function clickFunction(){
+var element=document.getElementById('myinput');
+//var div=document.getElementById('result');
+
+//div.firstChild.nodeValue=element.value;
+alert(element.value);
+}
+function myLoadFunction()
+{
+var element=document.getElementById('mybutton');
+element.addEventListener('click',clickFunction);
+}
+document.addEventListener('DOMContentLoaded',myLoadFunction);
